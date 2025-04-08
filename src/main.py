3d816688d9e507ecaed8f1927405be6019325589
Vladislav <barabayne@gmail.com>
2025-04-08 20:57:49 +0500

    🔧 chore: add lifespan

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-app  = FastAPI()
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    yield
+    
+app  = FastAPI(lifespan=lifespan)
 
 origins = [
     "http://localhost",
